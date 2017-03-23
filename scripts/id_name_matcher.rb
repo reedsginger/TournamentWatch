48abc5f219b7a2ca55a8aabb66ad6428c64e382a
reedsginger <naxicon@gmail.com>
2017-03-23 13:59:24 -0400

    Added support for single ticks may need support for double ticks

--- scripts/id_name_matcher.rb
+++ scripts/id_name_matcher.rb
@@ -8,16 +8,17 @@ link = "http://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key=#
 live_games = open(link)
 games = live_games.read
 gamedb = JSON.parse(games)
-conn = PG.connect(dbname: '', password: '' )
+conn = PG.connect(dbname: '', password: ' ' )
 
 gamedb['result']['games'].each do |game|
   game['players'].each do |player|
     if player['team'] == 0 || player['team'] == 1
       player_id = conn.exec("SELECT * FROM id_name WHERE account_id = #{player['account_id']}").values
       player_id.flatten!
-      valid = (player['name'] =~ /[^a-zA-Z0-9]/).nil?
-      if player_id.size == 0 && valid
-        conn.exec("INSERT INTO id_name VALUES(#{player['account_id']},\'#{player['name']}\')")
+      name = player['name']
+      valid_name = name.gsub('\'', "\\\'")
+      if player_id.size == 0
+        conn.exec("INSERT INTO id_name VALUES(#{player['account_id']},\'#{valid_name}\')")
       end
     end
   end
